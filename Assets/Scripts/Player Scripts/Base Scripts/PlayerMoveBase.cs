ffe08ecfb7cfd7d797de466b5663fc4118641a5d
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2025-02-26 17:59:01 +1100

    Improved Aim (fixed clamping)
    Player camera can look straight up and down
    Minor issue needs to be fixed: player can shoot themselves when looking straight down

--- Assets/Scripts/Player Scripts/Base Scripts/PlayerMoveBase.cs
+++ Assets/Scripts/Player Scripts/Base Scripts/PlayerMoveBase.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,33 +11,31 @@ public class PlayerMoveBase : MonoBehaviour
     //Controller
     private InputDevice thisController;
     private PlayerController controllerScript;
-
-    //Movement variables
-    private Rigidbody rb;
-    private Vector3 moveDir;
-    private float moveX;
-    private float moveZ;
-
-    public float moveSpeed;
-    public float jumpHeight;
-
-    //Camera variables
-    private float mouseX;
-    private float mouseY;
-    private float cameraPitch;
-    private float cameraYaw;
-    private float minClamp = 30f;
-    private float maxClamp = 120f;
-
-    public Camera playerCam;
+    
+    //Inputs
+    private float inputX;
+    private float inputY;
     public float mouseSens;
     public float controlXSens;
     public float controlYSens;
 
-    public Vector3 cameraOffset;
-    public float rightOffset;
-    public float heightOffset;
-    public float smoothSpeed;
+    //Movement variables
+    private Rigidbody rb;
+    private Vector3 moveDir;
+    public float moveSpeed;
+    public float jumpHeight;
+
+    //Camera variables
+    public Camera playerCam;
+    private float cameraPitch;
+    private float cameraYaw;
+    private float minClamp = -89.0f;
+    private float maxClamp = 89.0f;
+
+    //Camera offsests
+    public float distance;
+    public float offsetDistance;
+    public float heightOffset; 
 
     public enum playerState
     {
@@ -47,21 +46,20 @@ public class PlayerMoveBase : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-
         currentState = playerState.Idle;
+
+        rb = GetComponent<Rigidbody>();
+        controllerScript = gameObject.GetComponent<PlayerController>();
+        thisController = controllerScript.GetController();
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-        controllerScript = gameObject.GetComponent<PlayerController>();
-        thisController = controllerScript.GetController();
     }
 
     // Update is called once per frame
     void Update()
     {
         MoveDirection();
-
         HandleCamera();
     }
 
@@ -92,6 +90,8 @@ public class PlayerMoveBase : MonoBehaviour
 
     void MoveDirection()
     {
+        float moveX = 0;
+        float moveZ = 0;
         //gets vertical and horizontal input from the input device
         if (thisController is Keyboard keyboard)
         {
@@ -104,9 +104,7 @@ public class PlayerMoveBase : MonoBehaviour
             moveX = controller.leftStick.right.isPressed ? 1 : controller.leftStick.left.isPressed ? -1 : 0;
         }
 
-        //move direction is based on the direction the camera is facing
-        moveDir = playerCam.transform.TransformDirection(new Vector3(moveX, 0f, moveZ).normalized);
-        //Stops players from running into the air
+        moveDir = transform.TransformDirection(new Vector3(moveX, 0f, moveZ).normalized);
         moveDir.y = 0;
 
         if (moveDir.z > 0 || moveDir.x > 0)
@@ -131,35 +129,30 @@ public class PlayerMoveBase : MonoBehaviour
         if (thisController is Keyboard)
         {
             Mouse mouse = Mouse.current;
-            mouseX = mouse.delta.x.ReadValue() * mouseSens;
-            mouseY = mouse.delta.y.ReadValue() * mouseSens;
+            inputX = mouse.delta.x.ReadValue() * mouseSens;
+            inputY = mouse.delta.y.ReadValue() * mouseSens;
         }
         else if (thisController is Gamepad controller)
         {
-            mouseX = controller.rightStick.ReadValue().x * controlXSens;
-            mouseY = controller.rightStick.ReadValue().y * controlYSens;
+            inputX = controller.rightStick.ReadValue().x * controlXSens;
+            inputY = controller.rightStick.ReadValue().y * controlYSens;
 
         }
 
-        cameraYaw += mouseX;
-        cameraPitch += mouseY;
-
-        //stops the player from looking too high or too low
+        // Update yaw and pitch
+        cameraYaw += inputX;
+        cameraPitch -= inputY;
         cameraPitch = Mathf.Clamp(cameraPitch, minClamp, maxClamp);
 
-        //Rotate the camera around the player
+        // Update camera position and rotation
         Quaternion rotation = Quaternion.Euler(cameraPitch, cameraYaw, 0);
-        Vector3 newPos = transform.position + rotation * cameraOffset;
-
-        playerCam.transform.position = newPos;
-
-        //rotate the character the face the x direction of the camera
+        Vector3 targetPos = transform.position + transform.right * offsetDistance + Vector3.up * heightOffset;
+        Vector3 cameraPos = rotation * new Vector3(0, 0, -distance) + targetPos;
+        playerCam.transform.position = cameraPos;
+        playerCam.transform.rotation = rotation;
+        playerCam.transform.LookAt(transform.position + transform.right * offsetDistance + Vector3.up * heightOffset);
+        
+        // Rotate player horizontally to face direction of the camera's yaw
         transform.rotation = Quaternion.Euler(0, cameraYaw, 0);
-
-        //offsets ensure that the character isn't positioned in the middle of the screen for the players POV
-        Vector3 rightOffsetVec = playerCam.transform.right * rightOffset;
-        Vector3 heightOffsetVec = playerCam.transform.up * heightOffset;
-        //ensure that the camera is always looking at the player
-        playerCam.transform.LookAt(transform.position + rightOffsetVec + heightOffsetVec);
     }
 }
