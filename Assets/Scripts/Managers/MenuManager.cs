f2a2058852c72805380601dde365991553fb5237
Dylan Archer <111679436+Dylan-KA@users.noreply.github.com>
2025-03-02 23:02:01 +1100

    Updated Menu Mockup

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Linq;
+using UnityEngine.InputSystem.Controls;
+
+
+public class MenuManager : MonoBehaviour
+{
+    private GameObject currentMenu;
+
+    public GameObject mainMenu;
+    public GameObject characterMenu;
+    public GameObject gamemodeMenu;
+
+    private GameObject sceneManager;
+    private SceneManagement sceneManagement;
+    private GameObject playerManager;
+    private PlayerManager playerManagerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMenu = mainMenu;
+        mainMenu.SetActive(true);
+        characterMenu.SetActive(false);
+        gamemodeMenu.SetActive(false);
+
+        sceneManager = GameObject.Find("SceneManager");
+        sceneManagement = sceneManager.GetComponent<SceneManagement>();
+
+        playerManager = GameObject.Find("Player Manager");
+        playerManagerScript = playerManager.GetComponent<PlayerManager>();
+        playerManagerScript.inputDevices.Clear();
+        playerManagerScript.p1Controller = null;
+        playerManagerScript.p2Controller = null;
+        playerManagerScript.p3Controller = null;
+        playerManagerScript.p4Controller = null;
+        playerManagerScript.playerCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentMenu == mainMenu) { HandleMainMenu(); }
+        else if (currentMenu == characterMenu) { HandleCharacterMenu(); }
+        else if (currentMenu == gamemodeMenu) { HandleGamemodeMenu(); }
+    }
+
+    private void HandleMainMenu()
+    {
+        if ((Keyboard.current != null && Keyboard.current.enterKey.wasPressedThisFrame) ||
+            (Gamepad.current != null && Gamepad.current.aButton.wasPressedThisFrame))
+        {
+            LoadCharacterSelectionScreen();
+        }
+
+    }
+
+    private void HandleCharacterMenu()
+    {
+        if ((Keyboard.current != null && Keyboard.current.enterKey.wasPressedThisFrame) ||
+            (Gamepad.current != null && Gamepad.current.aButton.wasPressedThisFrame))
+        {
+            LoadGamemodeMenuScreen();
+        }
+    }
+
+    private void HandleGamemodeMenu()
+    {
+
+    }
+
+    public void LoadMainMenuScreen()
+    {
+        // Need to reset joined players & other menus
+        currentMenu = mainMenu;
+        
+        mainMenu.SetActive(true);
+        characterMenu.SetActive(false);
+        gamemodeMenu.SetActive(false);
+    }
+
+    public void LoadCharacterSelectionScreen()
+    {
+        currentMenu = characterMenu;
+
+        mainMenu.SetActive(false);
+        characterMenu.SetActive(true);
+        gamemodeMenu.SetActive(false);
+    }
+
+    public void LoadGamemodeMenuScreen()
+    {
+        currentMenu = gamemodeMenu;
+
+        mainMenu.SetActive(false);
+        characterMenu.SetActive(false);
+        gamemodeMenu.SetActive(true);
+    }
+
+}
